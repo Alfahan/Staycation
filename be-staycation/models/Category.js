a3efa729ea02eeeba1f1d26b342d3a576a7f6fb2
alfahan <ali.farhan160@gmail.com>
2022-03-23 12:58:46 +0700

    Add Model Activity, Category, Feature, Image, Item

--- be-staycation/models/Category.js
+++ be-staycation/models/Category.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-    name: { type: String }
+    name: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Categroy', categorySchema)
