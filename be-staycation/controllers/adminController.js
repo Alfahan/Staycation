310489c2aa75703331e0ab7046c16061da79c6f6
alfahan <ali.farhan160@gmail.com>
2022-03-28 11:06:54 +0700

    Delete bank with Image

--- be-staycation/controllers/adminController.js
+++ be-staycation/controllers/adminController.js
@@ -122,11 +122,26 @@ module.exports = {
                 bank.nomorRekening = nomorRekening;
                 bank.imageUrl = `images/${req.file.filename}`;
                 await bank.save();
-                req.flash('alertMessage', 'Success Add Bank');
+                req.flash('alertMessage', 'Success Update Bank');
                 req.flash('alertStatus', 'success');
                 res.redirect('/admin/bank');
             }
         } catch (err) {
+            req.flash('alertMessage', `${err.message}`);
+            req.flash('alertStatus', 'danger');      
+            res.redirect('/admin/bank');    
+        }
+    },
+    deleteBank: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const bank = await Bank.findOne({ _id:id });
+            await fs.unlink(path.join(`public/${bank.imageUrl}`));
+            await bank.remove();
+            req.flash('alertMessage', 'Success Delete Bank');
+            req.flash('alertStatus', 'success');
+            res.redirect('/admin/bank');
+        } catch (err) {    
             req.flash('alertMessage', `${err.message}`);
             req.flash('alertStatus', 'danger');      
             res.redirect('/admin/bank');    
