0dc91de36964c92b36b70487a71bb50a35884936
Alfahan <ali.farhan160@gmail.com>
2022-05-03 21:57:01 +0700

    Minor

--- be-staycation/controllers/adminController.js
+++ be-staycation/controllers/adminController.js
@@ -249,44 +249,43 @@ module.exports = {
     },
     addItem: async (req, res) => {
         try {
-            const {categoryId, title, price, city, about} = req.body;
-            if(req.files.length > 0){
-                const category = await Category.findOne({ _id: categoryId });
-                const newItem = {
-                    categoryId: category._id,
-                    title,
-                    description: about,
-                    price,
-                    city
-                }
-                const item = await Item.create(newItem);
-                category.itemId.push({ _id: item._id });
-                await category.save();
-                for (let i = 0; i < req.files.length; i++) {
-                    const imagesave = await Image.create({ imageUrl: `images/${req.files[i].filename}` });
-                    item.imageId.push({ _id: imagesave._id });
-                    await item.save();
-                }
-                req.flash('alertMessage', 'Success Add Item');
-                req.flash('alertStatus', 'success');
-                return res.redirect('/admin/item');
+            const { categoryId, title, price, city, about } = req.body;
+            if (req.files.length > 0) {
+              const category = await Category.findOne({ _id: categoryId });
+              const newItem = {
+                categoryId,
+                title,
+                description: about,
+                price,
+                city
+              }
+              const item = await Item.create(newItem);
+              category.itemId.push({ _id: item._id });
+              await category.save();
+              for (let i = 0; i < req.files.length; i++) {
+                const imageSave = await Image.create({ imageUrl: `images/${req.files[i].filename}` });
+                item.imageId.push({ _id: imageSave._id });
+                await item.save();
+              }
+              req.flash('alertMessage', 'Success Add Item');
+              req.flash('alertStatus', 'success');
+              res.redirect('/admin/item');
             }
-        } catch (err) {
-            req.flash('alertMessage', `${err.message}`);
-            req.flash('alertStatus', 'danger');      
-            return res.redirect('/admin/item');                
-        }
+          } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/admin/item');
+          }
     },
     showImageItem: async (req, res) => {
         try{
             const { id } = req.params;
             const item = await Item.findOne({ _id: id })
                 .populate({ path:'imageId', select:'id imageUrl' });
-            
             const alertMessage = req.flash('alertMessage');
             const alertStatus = req.flash('alertStatus');
     
-            const alert = {message: alertMessage, status: alertStatus};
+            const alert = { message: alertMessage, status: alertStatus };
     
             return res.render('admin/item/view_item',{ 
                 title: "Staycation | Show Image Item",
--- be-staycation/models/Item.js
+++ be-staycation/models/Item.js
@@ -9,6 +9,7 @@ const itemSchema = new mongoose.Schema({
     isPopular: { type: Boolean, default:false },
     description: { type: String, required: true },
     unit: { type: String, default:'night'  },
+    sumBooking: { type: Number, default: 0 },
     categoryId: { type: ObjectId, ref: 'Category' },
     imageId: [{ type: ObjectId, ref: 'Image' }],
     featureId: [{ type: ObjectId, ref: 'Feature' }],
