1e226fda38fc4ee9a96d9c9c4bb15bcbddd85c11
Alfahan <ali.farhan160@gmail.com>
2022-05-04 13:52:44 +0700

    Endpoint Booking Page

--- be-staycation/models/Booking.js
+++ be-staycation/models/Booking.js
@@ -53,7 +53,7 @@ const bookingSchema = new mongoose.Schema({
         },
         status: {
             type: String,
-            required: false
+            default: 'Proses'
         }
     }
 })
