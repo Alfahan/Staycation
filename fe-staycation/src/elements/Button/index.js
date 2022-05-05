4498eacff1341e8d79f868dad8c5cbd730a351a0
Alfahan <ali.farhan160@gmail.com>
2022-05-05 18:42:56 +0700

    Header Landing Page

--- fe-staycation/src/elements/Button/index.js
+++ fe-staycation/src/elements/Button/index.js
@@ -34,27 +34,26 @@ export default function Button(props) {
   }
 
 
-  if(props.type === 'Link'){
+  if(props.type === 'link'){
     if(props.isExternal) {
-      return (
+      return  (
+        // eslint-disable-next-line react/jsx-no-target-blank
         <a 
-          href={props.href} 
-          className={className.join(' ')} 
-          style={props.style} 
-          target={props.target === "_blank" ? "_blank":undefined} 
-          rel={props.target === "_blank" ? "noopener noreferrer":undefined}
+        href={props.href} 
+        className={className.join(" ")} 
+        style={props.style} 
+        target={props.target === "_blank" ? "_blank" : undefined} 
+        rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
         >
-          {props.children}
-        </a>
+            {props.children}</a>
       )
     } else {
       return (
-        <Link to={props.href}
-          className={className.join(' ')}
-          style={props.style} 
-          onClick={onClick}
-        >
-          {props.children}
+        <Link 
+        to={props.href} 
+        className={className.join(" ")} 
+        style={props.style} onClick={onClick}>
+            {props.children}
         </Link>
       )
     }
--- fe-staycation/src/elements/Button/index.test.js
+++ fe-staycation/src/elements/Button/index.test.js
@@ -14,19 +14,18 @@ test('Should render loading/spinner', () => {
     const { container, getByText } = render(<Button isLoading></Button>);
 
     expect(getByText(/loading/i)).toBeInTheDocument();
-
-    expect(container.querySelector('span')).toBeInTheDocument();
+    expect(container.querySelector('span')).toBeInTheDocument()
 });
 
 test('Should render <a> tag', () => {
-    const { container } = render(<Button type="Link" isExternal></Button>);
+    const { container } = render(<Button type="link" isExternal></Button>);
 
     expect(container.querySelector('a')).toBeInTheDocument();
 });
 
 test('Should render <Link> component', () => {
     const { container } = render(<Router>
-            <Button href="" type="Link" ></Button>
+            <Button href="" type="link" ></Button>
         </Router>
     );
 
